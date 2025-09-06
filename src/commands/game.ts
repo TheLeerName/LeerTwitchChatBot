686f2639906efd2500cb7962cd89891755d42e1f
Leer <85291330+TheLeerName@users.noreply.github.com>
2025-09-06 18:16:01 +0700

    send game name on !game command

--- src/commands/game.ts
+++ src/commands/game.ts
@@ -14,21 +14,20 @@ export default {
 			return res.twitch = `❌ Нет полномочий.`;
 
 		const a = authorization[req.message.payload.event.broadcaster_user_id];
-		const game = req.args.join(" ").toLowerCase();
-		var game_id: string | undefined;
-		if (game === "общение" || game === "just chatting") game_id = "509658";
-		else {
-			const response = await Twitch.Request.SearchCategories(a, game, 1);
-			res.log += `\tsearchcategories: ${JSON.stringify(response)}\n`;
-			if (!response.ok) return res.twitch = `❌ Ошибка! (${response.status} - ${response.message})`;
-			if (response.data.length === 0) return res.twitch = `❌ Игра не найдена!`;
-			game_id = response.data[0].id;
-		}
-		if (!game_id)
+		const query = req.args.join(" ").toLowerCase();
+
+		const searchcategories = await Twitch.Request.SearchCategories(a, query, 1);
+		res.log += `\tsearchcategories: ${JSON.stringify(searchcategories)}\n`;
+
+		if (!searchcategories.ok)
+			return res.twitch = `❌ Ошибка! (${searchcategories.status} - ${searchcategories.message})`;
+		if (searchcategories.data.length === 0)
 			return res.twitch = `❌ Игра не найдена!`;
 
-		const response = await Twitch.Request.ModifyChannelInformation(a, { game_id });
+		const game = searchcategories.data[0];
+
+		const response = await Twitch.Request.ModifyChannelInformation(a, { game_id: game.id });
 		res.log += `\tmodifychannelinformation: ${JSON.stringify(response)}\n`;
-		return res.twitch = response.ok ? `✅ Успешно!` : `❌ Ошибка! (${response.status} - ${response.message})`;
+		return res.twitch = response.ok ? `✅ Игра изменена на ${game.name}!` : `❌ Ошибка! (${response.status} - ${response.message})`;
 	}
 };
